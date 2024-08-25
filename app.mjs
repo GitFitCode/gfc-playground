56e6137671845025e6d10b7c91cae7c5bc640296
Miguel <contact@miguelalmodo.com>
2024-08-25 01:05:48 -0400

    added chalkjs and made the intro font red

---
+++ app.mjs
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import chalk from 'chalk';
+
+const program = new Command(); 
+
+program
+    .name('gfc-playground-cli')
+    .description("GFC' CLI will help you get started in GitFitCode")
+    .version('1.0')
+
+program
+  .command('playground-intro')
+  .description('Introduce yourself so you can enter the GitFitCode playground! What are you waiting for?')
+  .argument('<string>', 'whats your discord handle?')
+  .option('--n <name>', 'What should we call you?')
+  .action((userInput, option) => {
+    console.log(chalk.red(`Woah there...we have found a new sparring partner ${userInput}`));
+  })
+
+program.parse(process.args)
+program.showHelpAfterError('(add --help for additional information)');
