783bbc37e866d2e7c389483261ab4933dde9583c
Miguel <contact@miguelalmodo.com>
2024-08-26 17:51:24 -0400

    added functionality for an ascii gfc logo

--- app.mjs
+++ app.mjs
@@ -1,16 +1,22 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import chalk from 'chalk';
 import chalkAnimation from 'chalk-animation';
+import Image from 'ascii-art-image';
+
 
 const program = new Command(); 
+
+var image = new Image({
+  filepath: 'gfc_logo.png',
+  alphabet: 'variant4'
+});
 
 program
     .name('gfc-playground-cli')
     .description('GFC CLI will help you get started in GitFitCode')
     .version('0.1.0')
-
+  
 program
   .command('playground-intro')
   .description('Introduce yourself so you can enter the GitFitCode playground! What are you waiting for?')
@@ -24,5 +30,24 @@ program
   
  })
 
-program.parse(process.args)
-program.showHelpAfterError('(add --help for additional information)');
+function introMessage() {
+  let str = "Welcome to GitFitCode! We are glad you are here (="
+  let str2 = "This command line interface tool will help orient you to your learning journey!"
+  const rainbow = chalkAnimation.rainbow(str + '\n' + str2);
+
+  // this outputs a ascii image of the gfc logo
+ /* image.write(function(err, rendered){
+    console.log(rendered);
+}) */ 
+
+// this outputs a rainbow welcome message 
+    setTimeout(() => {
+    rainbow.stop();
+    program.outputHelp();
+  }, 2000)
+}
+
+introMessage();
+
+//program.parse(process.args)
+//program.showHelpAfterError('(add --help for additional information)');
